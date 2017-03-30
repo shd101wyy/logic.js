610a881bd49cb1821cb55738b7c46012cbee4b23
Yiyi Wang <shd101wyy@sina.com>
2017-03-30 01:01:31 -0500

    added facts and membero

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,19 @@
-const {lvar, run, eq, and, or} = require ('../lib/logic.js')
+const {lvar, run, eq, and, or, membero, numbero, stringo} = require ('../lib/logic.js')
 
 const x = lvar('x')
 const y = lvar('y')
+/*
 console.log(
   run(2, [x, y], or(
     and(eq(x, y), eq(x, 3)),
     eq(x, 2)))
+)*/
+/*
+console.log(
+  run([x], membero(x, [1, 2, 3]))
+)
+*/
+
+console.log(
+  run([x], and(stringo(x), eq(x, 'abc')))
 )
